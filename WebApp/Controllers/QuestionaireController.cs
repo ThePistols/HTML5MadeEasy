5a1939f68984d765f25f0d6b3f3c6380f4ce76b8
Jamie <Firenyth@gmail.com>
2017-06-30 00:02:06 +1200

    user quiz's now visible on teacher page

--- WebApp/Controllers/QuestionaireController.cs
+++ WebApp/Controllers/QuestionaireController.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -195,8 +201,236 @@ namespace WebApp.Controllers
                 ViewData["Answer17Result"] = "/Content/Images/x mark.png";
 
             }
+
+            var userManager = new UserManager<ApplicationUser>(
+            new UserStore<ApplicationUser>(new ApplicationDbContext())
+            );
+
+            var temp = userManager.FindByName(User.Identity.Name);
+            var userID = temp.Id;
+
+
+            string connStr = ConfigurationManager.ConnectionStrings["Questions"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string sql = "INSERT INTO [quiz] (UserID, DateTime, answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10, answer11, answer12, answer13, answer14, answer15, answer16, answer17) Values (@userid, @datetime, @ans1, @ans2, @ans3, @ans4, @ans5, @ans6, @ans7, @ans8, @ans9, @ans10, @ans11, @ans12, @ans13, @ans14, @ans15, @ans16, @ans17)";
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.Add("@userid", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@datetime", SqlDbType.DateTime);
+                cmd.Parameters.Add("@ans1", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans2", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans3", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans4", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans5", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans6", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans7", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans8", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans9", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans10", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans11", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans12", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans13", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans14", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans15", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans16", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ans17", SqlDbType.NVarChar);
+
+                cmd.Parameters["@userid"].Value = userID;
+                cmd.Parameters["@datetime"].Value = DateTime.Now;
+                if (Answer1 != null)
+                {
+                    cmd.Parameters["@ans1"].Value = Answer1;
+                }
+                else
+                {
+                    cmd.Parameters["@ans1"].Value = "";
+                }
+
+                if (Answer2 != null)
+                {
+                    cmd.Parameters["@ans2"].Value = Answer2;
+                }
+                else
+                {
+                    cmd.Parameters["@ans2"].Value = "";
+                }
+
+                if (Answer3 != null)
+                {
+                    cmd.Parameters["@ans3"].Value = Answer3;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans3"].Value = "";
+
+                }
+
+                if (Answer4 != null)
+                {
+                    cmd.Parameters["@ans4"].Value = Answer4;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans4"].Value = "";
+
+                }
+
+                if (Answer5 != null)
+                {
+                    cmd.Parameters["@ans5"].Value = Answer5;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans5"].Value = "";
+
+                }
+
+                if (Answer6 != null)
+                {
+                    cmd.Parameters["@ans6"].Value = Answer6;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans6"].Value = "";
+
+                }
+
+                if (Answer7 != null)
+                {
+                    cmd.Parameters["@ans7"].Value = Answer7;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans7"].Value = "";
+
+                }
+
+                if (Answer8 != null)
+                {
+                    cmd.Parameters["@ans8"].Value = Answer8;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans8"].Value = "";
+
+                }
+
+                if (Answer9 != null)
+                {
+                    cmd.Parameters["@ans9"].Value = Answer9;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans9"].Value = "";
+
+                }
+
+                if (Answer10 != null)
+                {
+                    cmd.Parameters["@ans10"].Value = Answer10;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans10"].Value = "";
+
+                }
+
+                if (Answer11 != null)
+                {
+                    cmd.Parameters["@ans11"].Value = Answer11;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans11"].Value = "";
+
+                }
+
+                if (Answer12 != null)
+                {
+                    cmd.Parameters["@ans12"].Value = Answer12;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans12"].Value = "";
+
+                }
+
+                if (Answer13 != null)
+                {
+                    cmd.Parameters["@ans13"].Value = Answer13;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans13"].Value = "";
+
+                }
+
+                if (Answer14 != null)
+                {
+                    cmd.Parameters["@ans14"].Value = Answer14;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans14"].Value = "";
+
+                }
+
+                if (Answer15 != null)
+                {
+                    cmd.Parameters["@ans15"].Value = Answer15;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans15"].Value = "";
+
+                }
+
+                if (Answer16 != null)
+                {
+                    cmd.Parameters["@ans16"].Value = Answer16;
+
+                }
+                else
+                {
+                    cmd.Parameters["@ans16"].Value = "";
+    
+                }
+
+                if (Answer17 != null)
+                {
+                    cmd.Parameters["@ans17"].Value = Answer17;
+                }
+                else
+                {
+                    cmd.Parameters["@ans17"].Value = "";
+                }
+
+
+
+                //uncomment line below to execute the query
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+            }
+
+
             return View();
         }
+
 
     }
 }
--- WebApp/Controllers/TeacherController.cs
+++ WebApp/Controllers/TeacherController.cs
@@ -10,76 +10,76 @@ using Microsoft.AspNet.Identity;
 using WebApp.Models;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WebApp.Controllers
 {
     public class TeacherController : Controller
     {
-        // GET: Teacher
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         string teststring = "TEACHER VIEW ONLY";
-
-        private void seedDatabase()
-        {
-            var roleManager = new RoleManager<IdentityRole>(
-                new RoleStore<IdentityRole>(new ApplicationDbContext())
-            );
-
-            var userManager = new UserManager<ApplicationUser>(
-                new UserStore<ApplicationUser>(new ApplicationDbContext())
-            );
-
-            // get a list of existing application users
-
-            List<ApplicationUser> currentUsers = userManager.Users.ToList();
-            if (currentUsers != null)
-            {
-                var temp = new List<String>();
-                var temp2 = new List<String>();
-                //    temp.Add("START");
-                foreach (ApplicationUser currentUser in currentUsers)
-                {
-                    temp.Add(currentUser.UserName);
-                    //  temp2.Add(roleManager.FindById(currentUser.Id).ToString());
-                    //  roleManager.FindById(currentUser.Id).ToString();
-
-                }
-                //    temp.Add("END");
-                ViewBag.UserList = temp;
-                ViewBag.RoleList = temp2;
-                // ViewBag.UserRole = Userrole;
-            }
-
-            // get a list of existing application roles
-            List<IdentityRole> roles2 = roleManager.Roles.ToList();
-            if (roles2 != null)
-            {
-                foreach (IdentityRole roleuser in roles2)
-                {
-                    //  roleManager.FindById
-                    //   roleManager.Delete(roleuser);
-                }
-            }
-
-        }
-        public ActionResult Testing()
-        {
-            teststring = "NO LONGER HELLO :P";
-            seedDatabase();
-            ViewBag.teststring = teststring;
-            return View("teacher");
-        }
 
 
         public ActionResult TeacherIndex()
         {
             ViewBag.teststring = teststring;
+
+
+            var userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext())
+            );
+
+            var result = new List<String>();
+
+            string connStr = ConfigurationManager.ConnectionStrings["Questions"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                var temp = userManager.FindByName(User.Identity.Name);
+                string userID = temp.Id;
+                string sql = "SELECT * FROM [quiz]";
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                // cmd.Parameters.AddWithValue("@ID", userID);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+
+                        var userid1 = reader.GetString(1);
+                        var username1 = userManager.FindById(userid1);
+
+                        result.Add(username1.UserName);
+                        result.Add(reader.GetSqlDateTime(2).ToString());
+                        result.Add(reader.GetString(3)); //ans 1
+                        result.Add(reader.GetString(4)); //ans 2
+                        result.Add(reader.GetString(5)); //ans 3
+                        result.Add(reader.GetString(6)); //ans 4
+                        result.Add(reader.GetString(7)); //ans 5
+                        result.Add(reader.GetString(8)); //ans 6
+                        result.Add(reader.GetString(9)); //ans 7
+                        result.Add(reader.GetString(10)); //ans 8
+                        result.Add(reader.GetString(11)); //ans 9
+                        result.Add(reader.GetString(12)); //ans 10
+                        result.Add(reader.GetString(13)); //ans 11
+                        result.Add(reader.GetString(14)); //ans 12
+                        result.Add(reader.GetString(15)); //ans 13
+                        result.Add(reader.GetString(16)); //ans 14
+                        result.Add(reader.GetString(17)); //ans 15
+                        result.Add(reader.GetString(18)); //ans 16
+                        result.Add(reader.GetString(19)); //ans 17
+                        
+
+                    }
+                }
+
+                conn.Close();
+            }
+            ViewBag.answers = result;
+
+
             return View("teacher");
         }
- 
+
     }
 }
